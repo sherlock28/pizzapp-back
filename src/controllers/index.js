dc170eab3023f34583c0c85ddab6c61eba8976bd
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-05-28 22:45:20 -0300

    The route for pizza post is added

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   productCtrl: {
     getAllPizzas: require("./product/getAllPizzas.ctrl"),
+    postPizza: require("./product/postPizza.ctrl"),
   },
   usersCtrl: {
     signUp: require("./users/signUp.ctrl"),
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const { productCtrl } = require('../controllers');
 
 router.get('/product', productCtrl.getAllPizzas);
+router.post('/product', productCtrl.postPizza);
 
 module.exports = router;
