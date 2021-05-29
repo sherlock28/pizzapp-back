eaf2a27eb06784b0194326e9314a13f3f351cf23
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-05-28 21:22:30 -0300

    User authentication paths are added

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  auth: {
-    tokenValidation: require("./auth/tokenValidation.mdw"),
-    fieldUserValidation: require("./auth/fieldUserValidation.mdw"),
+  users: {
+    tokenValidation: require("./users/tokenValidation.mdw"),
+    fieldUserValidation: require("./users/fieldUserValidation.mdw"),
   },
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
-const router = require('express').Router();
-const { usersCtrl } = require('../controllers');
+const router = require("express").Router();
+const { usersCtrl } = require("../controllers");
+const { users } = require("../middlewares");
 
+router.post("/signup", users.fieldUserValidation, usersCtrl.signUp);
 router.post("/signin", usersCtrl.signIn);
+router.post("/signout", users.tokenValidation, usersCtrl.signOut);
 
 module.exports = router;
