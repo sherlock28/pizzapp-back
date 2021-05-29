79c3ba75210622a15a389c21cbf295af43239535
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-05-28 22:34:15 -0300

    Controller is added to add pizzas

---
+++ src/controllers/product/postPizza.ctrl.js
@@ -0,0 +1,35 @@
+const { cloudinary } = require("../../config");
+const { Product } = require("../../models");
+const fs = require("fs-extra");
+
+const postProjects = async (req, res) => {
+  const { name, description, price } = req.body;
+
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path);
+
+    const newProduct = Project({
+      name,
+      description,
+      price,
+      imageURL: result.secure_url,
+      public_id: result.public_id,
+    });
+
+    const productSaved = await newProduct.save();
+
+    await fs.unlink(req.file.path);
+
+    res.status(201).json({
+      status: "Ok",
+      message: "Product successfully added",
+      data: {
+        product: productSaved,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ status: "Error", message: "Internal server error" });
+  }
+};
+
+module.exports = postProjects;
