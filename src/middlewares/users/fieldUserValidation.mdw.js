e1ab1e763cae22b2f5a5716a383b2f08493f6427
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-05-28 22:03:53 -0300

    Verification is added for the fullname field

--- src/middlewares/users/fieldUserValidation.mdw.js
+++ src/middlewares/users/fieldUserValidation.mdw.js
@@ -2,7 +2,22 @@ const fieldUserValidation = (req, res, next) => {
   const emailRegex =
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
 
-  const { username, email, password, confirmpass } = req.body;
+  const fullnameRegex = /^[A-ZÁÉÍÓÚÑ ]+$/i;
+
+  const { fullname, username, email, password, confirmpass } = req.body;
+
+  if (fullname === undefined || fullname.length === 0) {
+    res.status(400).json({ status: "Error", message: "Full name is required" });
+    return;
+  }
+
+  if (fullname.toString().match(fullnameRegex) === null) {
+    res.status(400).json({
+      status: "Error",
+      message: "The name can only contain letters (A-Z) or (a-z)",
+    });
+    return;
+  }
 
   if (username === undefined || username.length === 0) {
     res.status(400).json({ status: "Error", message: "Username is required" });
