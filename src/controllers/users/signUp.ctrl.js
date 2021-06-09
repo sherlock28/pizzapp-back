929e335dfb13a5146c3d62f4ccd8192ef4bed47d
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-06-08 22:36:44 -0300

    The phone field is added to the user

--- src/controllers/users/signUp.ctrl.js
+++ src/controllers/users/signUp.ctrl.js
@@ -4,12 +4,23 @@ const { encryptPassword } = require("../../libs");
 const signUp = async (req, res) => {
   const { fullname, username, email, password } = req.body;
 
-  const newUser = new User({
-    fullname,
-    username,
-    email,
-    password,
-  });
+  let newUser;
+  if (req.body.phone) {
+    newUser = new User({
+      fullname,
+      username,
+      email,
+      phone: req.body.phone,
+      password,
+    });
+  } else {
+    newUser = new User({
+      fullname,
+      username,
+      email,
+      password,
+    });
+  }
 
   const user = await User.findOne({ email: email }).lean();
 
--- src/models/User.model.js
+++ src/models/User.model.js
@@ -4,6 +4,7 @@ const UserSchema = new Schema({
   fullname: String,
   username: String,
   email: String,
+  phone: String,
   password: String,
   token: String,
 });
