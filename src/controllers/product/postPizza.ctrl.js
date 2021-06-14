e2b50a13bf75a17b2ca227537b017aecc4b5ae28
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-06-14 10:57:17 -0300

    Name correction

--- src/controllers/product/postPizza.ctrl.js
+++ src/controllers/product/postPizza.ctrl.js
@@ -3,16 +3,16 @@ const { Product } = require("../../models");
 const fs = require("fs-extra");
 
 const postProduct = async (req, res) => {
-  const { name, description, price, off, rating, reviewCount } = req.body;
+  const { name, description, price, rating, reviewCount } = req.body;
 
   try {
     const result = await cloudinary.uploader.upload(req.file.path);
 
-    const newProduct = Project({
+    const newProduct = Product({
       name,
       description,
       price,
-      off,
+      off: req.body.off || "",
       rating,
       reviewCount,
       imageURL: result.secure_url,
