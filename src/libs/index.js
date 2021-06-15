c8b57b10c1047b5303f9671a5d441a2c0a556e57
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-06-15 11:29:20 -0300

    Name correction

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   encryptPassword: require("./encryptPassword.libs"),
   validatePassword: require("./validatePassword.libs"),
-  createMailToVerifyAccount: require("./createMailToVerifyAccount"),
-  generateHash: require("./generateHash"),
+  createMailToVerifyAccount: require("./createMailToVerifyAccount.libs"),
+  generateHash: require("./generateHash.libs"),
 };
