25e95300c7deef7dfe6946ac6f105d8a0df44916
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-07-14 22:39:54 -0300

    Modified the toJSON of the Product model

--- src/models/Product.model.js
+++ src/models/Product.model.js
@@ -1,14 +1,25 @@
 const { model, Schema } = require("mongoose");
 
-const ProductSchema = new Schema({
-  name: String,
-  description: String,
-  price: Number,
-  off: Number,
-  rating: Number,
-  reviewCount: Number,
-  imageURL: String,
-  public_id: String,
-});
+const ProductSchema = new Schema(
+  {
+    name: String,
+    description: String,
+    price: Number,
+    off: Number,
+    rating: Number,
+    reviewCount: Number,
+    imageURL: String,
+    public_id: String,
+  },
+  {
+    toJSON: {
+      transform: function (document, returnedObject) {
+        returnedObject.id = returnedObject._id;
+        delete returnedObject._id;
+        delete returnedObject.__v;
+      },
+    },
+  }
+);
 
 module.exports = model("Product", ProductSchema);
