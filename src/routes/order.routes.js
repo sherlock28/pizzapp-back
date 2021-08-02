cc5619bfa8e0e137f36beccff177fe1ae08a0534
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-08-01 23:01:19 -0300

    Correction of resource names for the endpoint orders

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const { orderCtrl } = require("../controllers");
 // const { users, auth } = require("../middlewares");
 
-router.post("/order", orderCtrl.postOrder);
-router.get("/order", orderCtrl.getOrdersByUser);
-router.get("/order/:order_id", orderCtrl.getOrderById);
+router.post("/orders", orderCtrl.postOrder);
+router.get("/orders", orderCtrl.getOrdersByUser);
+router.get("/orders/:order_id", orderCtrl.getOrderById);
 
 module.exports = router;
