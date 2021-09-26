fa1209ffada34e70902d62ab91f8426198095e56
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-09-26 03:57:20 -0300

    Cart routes are added

---
+++ src/routes/cart.routes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+// const { cartCtrl } = require("../controllers");
+// const { users, auth } = require("../middlewares");
+
+// router.post("/cart", cartCtrl.addItemToCart);
+// router.get("/cart", orderCtrl.getCart);
+// router.delete("/cart/:id", orderCtrl.deleteItemFromCart);
+// router.delete("/cart/empty/:id", orderCtrl.emptyCart);
+
+module.exports = router;
