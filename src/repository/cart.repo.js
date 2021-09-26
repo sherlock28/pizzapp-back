fcb4bacd39103ac80d15bee12f5d0fe767f6b153
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-09-26 03:59:35 -0300

    Cart repository is added

---
+++ src/repository/cart.repo.js
@@ -0,0 +1,14 @@
+const { Cart } = require("../models");
+
+exports.cart = async () => {
+  const carts = await Cart.find().populate({
+    path: "items.productId",
+    select: "name price total",
+  });
+  return carts[0];
+};
+
+exports.addItem = async (payload) => {
+  const newItem = await Cart.create(payload);
+  return newItem;
+};
