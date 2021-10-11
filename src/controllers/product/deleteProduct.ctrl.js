128bf1efb1edeb925465617f9040339d148f0d71
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-10-11 08:43:43 -0300

    The repository is used to remove a product

--- src/controllers/product/deleteProduct.ctrl.js
+++ src/controllers/product/deleteProduct.ctrl.js
@@ -1,11 +1,11 @@
 const { cloudinary } = require("../../config");
-const { Product } = require("../../models");
+const productRepository = require("../../repository/product.repo");
 
 const deleteProduct = async (req, res) => {
   try {
     const { id_product } = req.params;
 
-    const product = await Product.findByIdAndRemove(id_product);
+    const product = await productRepository.removeProduct(id_product);
 
     const result = await cloudinary.uploader.destroy(product.public_id, {
       folder: process.env.CLOUDINARY_FOLDER,
@@ -14,7 +14,7 @@ const deleteProduct = async (req, res) => {
     res.json({ status: "Ok", message: "Product successfully deleted" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ status: "Error", message: "Internal server error" });
+    res.status(500).json({ status: "Error", message: "Product could not be removed" });
   }
 };
 
