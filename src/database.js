1455d54d4370705a9ed489a022589a94c37fa185
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-10-11 05:46:22 -0300

    Database connection closed on exit

--- src/database.js
+++ src/database.js
@@ -11,8 +11,20 @@ mongoose
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-  .then(db => console.log("db is connected to", db.connection.name))
-  .catch(err => {
+  .then((db) => console.log("db is connected to", db.connection.name))
+  .catch((err) => {
     console.error(err);
     console.error("database connection failed");
   });
+
+function cleanup() {
+  mongoose.connection.close(function () {
+    console.log("\nMongoose default connection disconnected through app termination");
+    console.log("\nGoodbye!");
+    process.exit(0);
+  });
+}
+
+process.on("SIGINT", cleanup);
+process.on("SIGTERM", cleanup);
+process.on("SIGHUP", cleanup);
