b1898816756b7151b6ab1b319846285480249d08
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-10-19 21:03:05 -0300

    getProductsOfCart added to product repository

--- src/repository/product.repo.js
+++ src/repository/product.repo.js
@@ -29,3 +29,13 @@ exports.removeProduct = async (id) => {
   const product = await Product.findByIdAndRemove(id);
   return product;
 };
+
+exports.getProductsOfCart = async (products) => {
+  const productsIds = products.map((p) => p.id);
+  const promises = productsIds.map((id) => {
+    return Product.findById(id)
+      .then((product) => product)
+      .catch((err) => err);
+  });
+  return await Promise.all(promises);
+};
