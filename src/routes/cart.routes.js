1a386806a3fd5f561250a8d1866ed63c6a22cb2b
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-10-19 21:00:41 -0300

    Route getCartByUserId added

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -3,7 +3,7 @@ const { cartCtrl } = require("../controllers");
 // const { users, auth } = require("../middlewares");
 
 router.post("/cart", cartCtrl.addItemToCart);
-// router.get("/cart/:id_user", cartCtrl.getCartByUserId);
+router.get("/cart/:id_user", cartCtrl.getCartByUserId);
 // router.delete("/cart/:id", orderCtrl.deleteItemFromCart);
 // router.delete("/cart/empty/:id", orderCtrl.emptyCart);
 
